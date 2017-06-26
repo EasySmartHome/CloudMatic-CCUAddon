e573f82da1bd2f389daeba3bc5dfaa2b633c614a
Jens Maus <mail@jens-maus.de>
2017-06-26 18:02:57 +0200

    applied patch to automatically identify if addon is running on RaspberryMatic or a CCU2.

--- startup.sh
+++ startup.sh
@@ -17,15 +17,32 @@ if [ ! -e $USER_DIR ] ; then
   
   $BASE_DIR/install.tcl
 
+else
+
+  # make sure cloudmatic button is always present
+  if ! grep -q "ID mh CONFIG_NAME CloudMatic" /etc/config/hm_addons.cfg 2>/dev/null; then
+    $BASE_DIR/install.tcl
+  fi
+
 fi
 
-mkdir -p /dev/net
-mknod /dev/net/tun c 10 200
-insmod $BASE_DIR/tun.ko
+if [ ! -d /dev/net ]; then
+  mkdir -p /dev/net
+  if [ ! -e /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+    insmod $BASE_DIR/tun.ko
+  fi
+fi
 
-
-l1=`ifconfig | grep 'eth0' | tr -s ' ' | cut -d ' ' -f5 | tr ':' '-'`
-l2=`ifconfig | grep 'usb0' | tr -s ' ' | cut -d ' ' -f5 | tr ':' '-'` 
+if [[ -e /sys/devices/platform/ccu2-ic200 ]]; then
+  # this is for a CCU2
+  l1=`ifconfig | grep 'eth0' | tr -s ' ' | cut -d ' ' -f5 | tr ':' '-'`
+  l2=`ifconfig | grep 'usb0' | tr -s ' ' | cut -d ' ' -f5 | tr ':' '-'`
+else
+  # this is for RaspberryMatic
+  l1=`grep Serial /proc/cpuinfo | sed 's|Serial||' | sed 's|:||' | sed 's/^[ \t]*//'`
+  l2=`ifconfig | grep 'eth0' | tr -s ' ' | cut -d ' ' -f5 | tr ':' '-'`
+fi
 echo SerialNumber=$l1 > $USER_DIR/ids
 echo BidCoS-Address=$l2 >> $USER_DIR/ids
 
