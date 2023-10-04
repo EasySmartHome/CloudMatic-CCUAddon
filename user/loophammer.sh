6dd25bcee43c92f342cca4e9590084cbf36201cd
Eduard Schäfer <eschaefer@easy-smarthome.de>
2023-10-04 09:08:39 +0200

    Revert "add tls-cipher workaround until sha256 will be used for RaspberryMatic"
    This reverts commit 152a651a41cea85b27fa15bee7c8152805ad2d22.

--- user/loophammer.sh
+++ user/loophammer.sh
@@ -8,7 +8,7 @@
 #
 # Hammering-Schutz:
 # ==================
-# Das Skript kontrolliert, ob das aktuelle Datum nach dem Datum liegt, bis zu dem der Premium Zugang gï¿½ltig ist.
+# Das Skript kontrolliert, ob das aktuelle Datum nach dem Datum liegt, bis zu dem der Premium Zugang gültig ist.
 # Ist der Zugang abgelaufen, wird ein evtl. laufender openvpn Prozess beendet.
 # Dies ist notwendig, da die HomeMatic sonst permantent versucht eine Verbindung mit dem VPN Server aufzubauen.
 #
@@ -20,7 +20,6 @@
 # **************************************************************************************************************************************************
 #
 ADDONDIR=/usr/local/etc/config/addons/mh
-VERSION=`cat /VERSION |grep PRODUCT |awk -F"=" '{ print $2 }'|awk -F"_" '{print $1}'`
 
   #Dienst Status einlesen
   dienst=`/bin/busybox cat $ADDONDIR/dienst`
@@ -31,23 +30,13 @@ VERSION=`cat /VERSION |grep PRODUCT |awk -F"=" '{ print $2 }'|awk -F"_" '{print 
   # Teilbereich Hammering - Schutz
   # 
   nowdate=$(date +%s)
-  # Enddatum wird individuell gesetzt, in Abhï¿½ngigkeit der Schlï¿½ssel - Laufzeit
+  # Enddatum wird individuell gesetzt, in Abhängigkeit der Schlüssel - Laufzeit
   enddate=$(date -d "010123592010" +%s)
   if [ $enddate -ge $nowdate ] 
   then  
      #
-     # Aktuelles Datum ist vor dem Enddatum. VPN starten, falls es nicht lï¿½uft
+     # Aktuelles Datum ist vor dem Enddatum. VPN starten, falls es nicht läuft
      #
-      # Workaround RaspberryMatic
-     # https://github.com/jens-maus/RaspberryMatic/issues/2442
-     if [ "$VERSION" = "raspmatic" ]; then
-         if [ -f "/usr/local/etc/config/addons/mh/client.conf" ]; then
-             if [ ! "$(grep 'tls-cipher "DEFAULT:@SECLEVEL=0"' /usr/local/etc/config/addons/mh/client.conf)" ]; then
-                 echo 'tls-cipher "DEFAULT:@SECLEVEL=0"' >> $ADDONDIR/client.conf
-             fi
-         fi
-     fi
-     
      Processname=openvpn
      if [ ! -n "`pidof $Processname`" ] ; then  
         if [ $dienst -ge 1 ] ; then
@@ -76,7 +65,7 @@ VERSION=`cat /VERSION |grep PRODUCT |awk -F"=" '{ print $2 }'|awk -F"_" '{print 
     Processname=openvpn
     if [ -n "`pidof $Processname`" ] ; then  
       /bin/busybox logger -t homematic -p user.info "Keine meine-homematic.de VPN Lizenz aktiv, openvpn wird beendet."
-      /bin/busybox logger -t homematic -p user.info "Diest dient zum Schutz Ihrer HomeMatic, um permanente, ungï¿½ltige Verbindungsversuche alle 10 Sekunden zu unterbinden."
+      /bin/busybox logger -t homematic -p user.info "Diest dient zum Schutz Ihrer HomeMatic, um permanente, ungültige Verbindungsversuche alle 10 Sekunden zu unterbinden."
       dummy=`killall -9 openvpn`
     fi
 	Processname=nginx
@@ -87,7 +76,7 @@ VERSION=`cat /VERSION |grep PRODUCT |awk -F"=" '{ print $2 }'|awk -F"_" '{print 
  fi
 
   
-  #Wenn Dienst lï¿½uft beenden, falls Dienst = 0
+  #Wenn Dienst läuft beenden, falls Dienst = 0
     if [ $dienst -lt 1 ] ; then
 		Processname=openvpn
 		if [ -n "`pidof $Processname`" ] ; then  
